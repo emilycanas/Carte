8395f9cbba0e50cfa1f13858d3def0d36aae486f
Emily <107320022+emilycanas@users.noreply.github.com>
2022-09-26 13:47:43 -0700

    Add files via upload

---
+++ .NET/Interfaces/IStateService.cs
@@ -0,0 +1,11 @@
+﻿using Sabio.Models.Domain;
+using System.Collections.Generic;
+
+namespace Sabio.Services
+{
+    public interface IStateService
+    {
+        List<State> GetAllStates();
+        List<BaseState> GetAllStateCodes();
+    }
+}
